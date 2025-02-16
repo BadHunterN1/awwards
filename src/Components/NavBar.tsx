767442653f1700cf04a6f3663e697edaee1fca93
BadHunterN1 <ahmam2004@gmail.com>
2025-02-16 17:01:29 +0200

    enhancing mobile ui

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,96 +1,127 @@
-import {useEffect, useRef, useState} from "react";
+import { useEffect, useRef, useState } from "react";
 import Button from "./UI/Button.tsx";
-import {TiLocationArrow} from "react-icons/ti";
-import {useWindowScroll} from "react-use";
+import { TiLocationArrow } from "react-icons/ti";
+import { useWindowScroll } from "react-use";
 import gsap from "gsap";
-import SlideTaps from "./SlideTaps.tsx";
+import SlideTaps from "./UI/SlideTaps.tsx";
 
-export default  function NavBar() {
-    const [audioPlaying, setAudioPlaying] = useState(false);
-    const [isIndicatorActive, setIsIndicatorActive] = useState(false);
-    const [hasClicked, setHasClicked] = useState(false);
+export default function NavBar() {
+	const [audioPlaying, setAudioPlaying] = useState(false);
+	const [isIndicatorActive, setIsIndicatorActive] = useState(false);
+	const [hasClicked, setHasClicked] = useState(false);
 
-    const [lastScrollY, setLastScrollY] = useState(0);
-    const [isNavVisiable, setIsNavVisiable] = useState(true);
+	const [lastScrollY, setLastScrollY] = useState(0);
+	const [isNavVisiable, setIsNavVisiable] = useState(true);
 
-    const {y:currentScrollY} = useWindowScroll();
+	const { y: currentScrollY } = useWindowScroll();
 
-    useEffect(() => {
-        if (currentScrollY === 0 ){
-            setIsNavVisiable(true);
-            navContainerRef.current?.classList.remove('floating-nav');
-        }else if (currentScrollY > lastScrollY) {
-            setIsNavVisiable(false);
-            navContainerRef.current?.classList.add('floating-nav');
-        } else if (currentScrollY < lastScrollY) {
-            setIsNavVisiable(true);
-            navContainerRef.current?.classList.add('floating-nav');
-        }
+	useEffect(() => {
+		if (currentScrollY === 0) {
+			setIsNavVisiable(true);
+			navContainerRef.current?.classList.remove("floating-nav");
+		} else if (currentScrollY > lastScrollY) {
+			setIsNavVisiable(false);
+			navContainerRef.current?.classList.add("floating-nav");
+		} else if (currentScrollY < lastScrollY) {
+			setIsNavVisiable(true);
+			navContainerRef.current?.classList.add("floating-nav");
+		}
 
-        setLastScrollY(currentScrollY)
-    }, [currentScrollY, lastScrollY]);
+		setLastScrollY(currentScrollY);
+	}, [currentScrollY, lastScrollY]);
 
-    useEffect(() => {
-        gsap.to(navContainerRef.current, {
-            y: isNavVisiable? 0 : -100,
-            opacity: isNavVisiable? 1 : 0,
-            ease: 'power4.inOut',
-            duration: 0.2,
-        })
-    }, [isNavVisiable]);
+	useEffect(() => {
+		gsap.to(navContainerRef.current, {
+			y: isNavVisiable ? 0 : -100,
+			opacity: isNavVisiable ? 1 : 0,
+			ease: "power4.inOut",
+			duration: 0.2,
+		});
+	}, [isNavVisiable]);
 
-    const navContainerRef = useRef<HTMLDivElement>(null);
-    const audioRef = useRef<HTMLAudioElement>(null);
+	const navContainerRef = useRef<HTMLDivElement>(null);
+	const audioRef = useRef<HTMLAudioElement>(null);
 
-    const toggleAudio = () => {
-        setAudioPlaying(prev => !prev);
-        setIsIndicatorActive(prev => !prev);
-    }
+	const toggleAudio = () => {
+		setAudioPlaying((prev) => !prev);
+		setIsIndicatorActive((prev) => !prev);
+	};
 
-    useEffect(() => {
-    if (audioPlaying){
-        audioRef.current?.play();
-    }else  {
-        audioRef.current?.pause();
-    }
-    }, [audioPlaying]);
+	useEffect(() => {
+		if (audioPlaying) {
+			audioRef.current?.play();
+		} else {
+			audioRef.current?.pause();
+		}
+	}, [audioPlaying]);
 
-    useEffect(() => {
-        if (hasClicked) return;
+	useEffect(() => {
+		if (hasClicked) return;
 
-        const handleClick = () => {
-            setHasClicked(true);
-            audioRef.current?.play().catch((err) => console.error("Audio playback failed:", err));
-            toggleAudio();
-            window.removeEventListener("click", handleClick);
-        };
+		const handleClick = () => {
+			setHasClicked(true);
+			audioRef.current
+				?.play()
+				.catch((err) => console.error("Audio playback failed:", err));
+			toggleAudio();
+			window.removeEventListener("click", handleClick);
+		};
 
-        window.addEventListener("click", handleClick);
+		window.addEventListener("click", handleClick);
 
-        return () => {
-            window.removeEventListener("click", handleClick);
-        };
-    }, [hasClicked]);
+		return () => {
+			window.removeEventListener("click", handleClick);
+		};
+	}, [hasClicked]);
 
-    return (
-        <div ref={navContainerRef} className={'fixed inset-x-0 top-4 z-40 h-20 border-none transition-all duration-700 sm:inset-x-6'}>
-            <header className={'absolute top-1/2 w-full -translate-y-1/2 '}>
-                <nav className={'flex size-full items-center justify-between p-4'}>
-                    <div className={'flex items-center gap-7 '}>
-                        <img src={'/img/logo.png'} alt={'logo'} className={'w-10'}/>
-                        <Button id={'product-button'} title={'products'} rightIcon={ <TiLocationArrow/>} containerClass={'bg-blue-50 flex items-center justify-center gap-1'}/>
-                    </div>
-                        <div className={'flex h-full items-center'}>
-                            <SlideTaps/>
-                            <button onClick={toggleAudio} className={'ml-10 flex items-center space-x-0.5 py-2 cursor-pointer'}>
-                                <audio ref={audioRef} className={'hidden'} src={'/audio/loop.mp3'} loop/>
-                                {[1,2,3,4].map(bar => (
-                                    <div key={bar} style={{animationDelay: `${bar * 0.1}s`}}
-                                         className={`indicator-line ${isIndicatorActive? 'active' : ''}`}/>))}
-                            </button>
-                        </div>
-                </nav>
-            </header>
-        </div>
-    )
+	return (
+		<div
+			ref={navContainerRef}
+			className={
+				"fixed inset-x-0 top-4 z-40 h-20 border-none transition-all duration-700 sm:inset-x-6"
+			}>
+			<header className={"absolute top-1/2 w-full -translate-y-1/2 "}>
+				<nav className={"flex size-full items-center justify-between p-4"}>
+					<div className={"flex items-center gap-7 "}>
+						<img src={"/img/logo.png"} alt={"logo"} className={"w-10"} />
+						<Button
+							id={"product-button"}
+							title={"products"}
+							rightIcon={<TiLocationArrow />}
+							containerClass={
+								"hidden md:flex bg-blue-50 flex items-center justify-center gap-1"
+							}
+						/>
+					</div>
+					<div className={"flex h-full items-center"}>
+						<SlideTaps />
+						<a
+							className="md:hidden flex z-10 nav-hover-btn px-4 py-2 hover:bg-white hover:text-black rounded-2xl transition text-sm"
+							href={`#`}>
+							Home
+						</a>
+						<button
+							onClick={toggleAudio}
+							className={
+								"ml-10 flex items-center space-x-0.5 py-2 cursor-pointer"
+							}>
+							<audio
+								ref={audioRef}
+								className={"hidden"}
+								src={"/audio/loop.mp3"}
+								loop
+							/>
+							{[1, 2, 3, 4].map((bar) => (
+								<div
+									key={bar}
+									style={{ animationDelay: `${bar * 0.1}s` }}
+									className={`indicator-line ${isIndicatorActive ? "active" : ""}`}
+								/>
+							))}
+						</button>
+					</div>
+				</nav>
+			</header>
+		</div>
+	);
 }
---
+++ src/Components/UI/SlideTaps.tsx
@@ -0,0 +1,40 @@
+import Tap from "./Tap.tsx";
+import { motion } from "framer-motion";
+import { useState } from "react";
+import { TiLocationArrow } from "react-icons/ti";
+
+export default function SlideTaps() {
+	const [position, setPosition] = useState({
+		width: 0,
+		opacity: 0,
+		left: 0,
+	});
+	return (
+		<>
+			<div
+				onMouseLeave={() => {
+					setPosition((pv) => ({
+						...pv,
+						opacity: 0,
+					}));
+				}}
+				className={"hidden md:flex relative mx-auto w-fit rounded-full"}>
+				<Tap setPosition={setPosition}>
+					nexus <TiLocationArrow />
+				</Tap>
+				<Tap setPosition={setPosition}>
+					vault <TiLocationArrow />
+				</Tap>
+				<Tap setPosition={setPosition}>features</Tap>
+				<Tap setPosition={setPosition}>about</Tap>
+				<Tap setPosition={setPosition}>contact</Tap>
+				<motion.div
+					animate={position}
+					className={
+						"absolute z-0 h-5 w-36 rounded-full bg-white md:h-10 self-center"
+					}
+				/>
+			</div>
+		</>
+	);
+}
